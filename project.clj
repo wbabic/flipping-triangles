8674d6355c666c6fdd60147a9fea6dcb60b71037
Walter P Babic <wbabic@mac.com>
2014-01-06 18:21:38 -0700

    added an advanced profile for cljsbuild

--- project.clj
+++ project.clj
@@ -4,21 +4,25 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2030"] 
-                 [org.clojure/core.async "0.1.256.0-1bf8cf-alpha"]
+                 [org.clojure/clojurescript "0.0-2138"] 
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [com.cemerick/piggieback "0.1.2"]]
-  :profiles {:dev {:source-paths ["dev"]}}
   :plugins [[lein-cljsbuild "1.0.1"]]
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
-  :cljsbuild {
-              :builds [
-                       {:id "overview"
-                        :source-paths ["src/geometry"]
-                        :compiler {:output-to "resources/public/js/geometry.js"
-                                   :optimizations :whitespace
-                                   :pretty-print true}}
-                       {:id "debug"
-                        :source-paths ["src/geometry/connect"]
-                        :compiler {:output-to "resources/public/js/connect.js"
-                                   :optimizations :whitespace
-                                   :pretty-print true}}]})
+  :cljsbuild
+  {:builds
+   [{:id "dev"
+     :source-paths ["src/geometry"
+                    "src/dev"]
+     :compiler {:optimizations :whitespace
+                :pretty-print false
+                :output-to "resources/public/js/geometry.js"}}
+    {:id "advanced"
+     :source-paths ["src/geometry"]
+     :compiler {:optimizations :advanced
+                :pretty-print false
+                :output-to "resources/public/js/geometry-advanced.js"
+                :output-wrapper true}}]}
+  :profiles {:dev {:source-paths ["dev"]
+                   :dependencies [[org.clojure/tools.namespace "0.2.4"]
+                                  [org.clojure/java.classpath "0.2.0"]]}})
